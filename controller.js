c2a5fe0994d013354cf8867117f369eb0c9da6a1
Juan Pirir <pirirjuan86@gmail.com>
2023-06-13 19:49:55 -0600

    img

--- controller.js
+++ controller.js
@@ -1,28 +1,17 @@
-// Función para terminar el juego
-function endGame(message) {
-  // Mostrar mensaje de fin de juego
-  showMessage(message);
-
-  // Reiniciar la página
-  resetGame();
-}
-
 // Función para mostrar un mensaje en la interfaz
 function showMessage(message) {
+
   alert(message);
 }
-
 // Función para reiniciar el juego
 function resetGame() {
   // Reiniciar la página
   window.location.reload();
 }
-
 // Función para capturar eventos de teclado
 function handleKeyPress(event) {
   // Obtener la tecla presionada
   const key = event.key.toLowerCase();
-
   // Mover al agente en la dirección correspondiente
   if (key === 'w') {
     moveAgent('norte');
@@ -34,7 +23,6 @@ function handleKeyPress(event) {
     moveAgent('este');
   }
 }
-
 // Función para mover al agente en una dirección dada
 function moveAgent(direccion) {
   // Obtener la posición actual del agente
@@ -48,7 +36,6 @@ function moveAgent(direccion) {
       }
     }
   }
-
   // Calcular la nueva posición del agente según la dirección
   let newRow = currentRow;
   let newCol = currentCol;
@@ -61,21 +48,26 @@ function moveAgent(direccion) {
   } else if (direccion === 'este' && currentCol < boardSize - 1) {
     newCol++;
   }
-
   // Verificar si la nueva posición es válida y actualizar el tablero
   if (newRow !== currentRow || newCol !== currentCol) {
     // Realizar el movimiento actualizando el tablero
     board[currentRow][currentCol] = '-';
     board[newRow][newCol] = 'A';
     updateBoard();
-
     // Verificar las percepciones en la nueva posición
     const perceptions = perceive(newRow, newCol);
     console.log(perceptions);
-
     // Verificar si el juego debe terminar
     if (newRow === pitRow && newCol === pitCol) {
-      endGame('¡Has perdido! El agente ha caído en un pozo.');
+      const pitImg = document.createElement('img');
+      pitImg.src = 'imagen/pozo.jpg'; //  ruta de la imagen del pozo
+      pitImg.classList.add('pit');
+      const cellElement = document.getElementById(`cell-${newRow}-${newCol}`);
+      cellElement.appendChild(pitImg);
+      setTimeout(function() {
+        showMessage('¡Has perdido! El agente ha caído en un pozo.');
+        resetGame();
+      }, 0);
     } else if (newRow === wumpusRow && newCol === wumpusCol) {
       showMessage('¡Has perdido! El agente ha encontrado al Wumpus.');
       resetGame();
@@ -88,6 +80,5 @@ function moveAgent(direccion) {
     }
   }
 }
-
 // Agregar el evento de escucha para los eventos de teclado
 document.addEventListener('keydown', handleKeyPress);
